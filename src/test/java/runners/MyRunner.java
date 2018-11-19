4e445c06206d613c9d4743ec23684b57d62e7fcd
Tom Millichamp <tom@edgewords.co.uk>
2018-11-19 11:29:02 +0000

    amended report runner

--- src/test/java/runners/MyRunner.java
+++ src/test/java/runners/MyRunner.java
@@ -3,14 +3,15 @@ package runners;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+//import cucumber.api.junit.Cucumber;
 import com.hpe.alm.octane.OctaneCucumber;
 
 @RunWith (OctaneCucumber.class)
 @CucumberOptions(
-		plugin = {"html:reports/cucumber-html-report",
+		plugin = {"junit:reports/TestResult.xml",
+				"html:reports/cucumber-html-report",
 				"json:reports/cucumber.json",
-				"pretty", "junit:reports/TestResult.xml"},
+				},
 		//tags = {"@functional"},
 		features = {"src/test/java/features"},
 		glue = {"code_bindings"}
