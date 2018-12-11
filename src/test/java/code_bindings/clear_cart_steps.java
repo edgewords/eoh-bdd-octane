0803f4ec5da627c9ea497450500435f2c485f26f
Tom Millichamp <tom@edgewords.co.uk>
2018-12-11 10:50:51 +0000

    should fail.

--- src/test/java/code_bindings/clear_cart_steps.java
+++ src/test/java/code_bindings/clear_cart_steps.java
@@ -12,19 +12,19 @@ public class clear_cart_steps {
 	@Given("^There are items inthe shopping cart$")
 	public void there_are_items_inthe_shopping_cart() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    assertTrue(false);
 	}
 
 	@When("^I clear the cart$")
 	public void i_clear_the_cart() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		assertTrue(false);
 	}
 
 	@Then("^the cart should be empty$")
 	public void the_cart_should_be_empty() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		assertTrue(false);
 	}
 
 }
